4e4f3ad5fd5f47ccff7cb5526bdb919581e6b3ad
jhg <gusrbwjd1229@gmail.com>
2020-06-10 00:34:48 +0900

    0034

--- router/sr_router.c
+++ router/sr_router.c
@@ -77,7 +77,7 @@ int ip_black_list(struct sr_ip_hdr* iph)
   printf("blacklist: %d.%d.%d.%d\n", (ip_blacklist_int >> 24) & 255,
                                       (ip_blacklist_int >> 16) & 255,
                                       (ip_blacklist_int >> 8) & 255,
-                                      (ip_blacklist_int) & 255));
+                                      (ip_blacklist_int) & 255);
   if ((one & ((mask_int >> 24) & 255) == (ip_blacklist_int >> 24) & 255) 
     && (two & ((mask_int >> 16) & 255) == (ip_blacklist_int >> 16) & 255)
     && (three & ((mask_int >> 8) & 255) == (ip_blacklist_int >> 8) & 255)
